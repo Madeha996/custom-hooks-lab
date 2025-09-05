3aff8458d1834a4a9546b6e84615d7b5f972972e
madeha <madehayoussof96@gmail.com>
2025-09-05 09:45:33 +0300

    add routing and update style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router";
+import CounterEx from "./examples/counterEx";
+import ToggleEx from "./examples/toggleEx";
+import Button from "./components/button";
+
 const App = () => {
   return (
-    <div>
+    <div className="layout">
       <h1>Custom Hooks</h1>
+      <div className="buttons">
+        <Button title="useToggle" url="toggle" />
+        <Button title="useCounter" url="counter" />
+      </div>
+      <Routes>
+        <Route path="/toggle" element={<ToggleEx />} />
+        <Route path="/counter" element={<CounterEx />} />
+      </Routes>
     </div>
   );
 };
